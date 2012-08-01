cdd591cd9eff6a25a7b2fcbed95adde46e97830a
Lorefnon <lorefnon@gmail.com>
2012-08-01 11:00:42 +0530

    Create a rails application.
    Create a rails project skeleton and add some dependencies, in particular
    gems for rspec for testing and slim for templating.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Symposium::Application.config.secret_token = '7ec5cbb39228e93f0005e228700cb4a64683852c0f8c3752c2e975249bfd4541054ad4ced78312dbc87b46d19833166a16943d4eca5b16d1227cbde4978ee41e'
