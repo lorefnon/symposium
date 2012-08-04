e11036ef589c1d1e3de85566a7ac2f80797eacfc
Lorefnon <lorefnon@gmail.com>
2012-08-05 02:57:35 +0530

    Add new method to QuestionsController

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,10 @@
 class QuestionsController < ApplicationController
+  before_filter :authenticate_user! , :except => :index
+
   def index
-    
+  end
+
+  def new
+    @que = Question.new
   end
 end
