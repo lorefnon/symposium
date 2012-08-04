ad5395e0883bae43fc29365283d907637fdaec56
Lorefnon <lorefnon@gmail.com>
2012-08-05 02:49:12 +0530

    Add database fields for answers table.

--- db/migrate/20120803084650_create_answers.rb
+++ db/migrate/20120803084650_create_answers.rb
@@ -1,7 +1,12 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
-
+      t.text :body, :null => false
+      t.integer :creator_id, :null => false
+      t.integer :question_id, :null => false
+      t.integer :upvotes, :null => false
+      t.integer :downvotes, :null => false
+      t.boolean :is_flagged, :null => false
       t.timestamps
     end
   end
