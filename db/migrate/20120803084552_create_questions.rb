775cd7d420defa558877510fcffb1b6a2877827f
Lorefnon <lorefnon@gmail.com>
2012-08-05 02:48:44 +0530

    Add database fields for question

--- db/migrate/20120803084552_create_questions.rb
+++ db/migrate/20120803084552_create_questions.rb
@@ -1,7 +1,12 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
-
+      t.string :title, :null => false
+      t.text :description, :null => false
+      t.integer :creator_id, :null => false
+      # t.integer :upvotes
+      # t.integer :downvotes
+      t.boolean :is_closed, :null => false
       t.timestamps
     end
   end
