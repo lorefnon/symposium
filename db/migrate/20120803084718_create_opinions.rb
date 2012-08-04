32e24d15894309add72fda6de0992d69a27f7898
Lorefnon <lorefnon@gmail.com>
2012-08-05 02:49:30 +0530

    Add database fields for opinions table.

--- db/migrate/20120803084718_create_opinions.rb
+++ db/migrate/20120803084718_create_opinions.rb
@@ -1,7 +1,12 @@
 class CreateOpinions < ActiveRecord::Migration
   def change
     create_table :opinions do |t|
-
+      t.string :action, :null => false # upvote, downvote, flag
+      t.integer :creator_id, :null => false
+      t.integer :score_change, :null => false
+      t.integer :target_id, :null => false
+      t.integer :target_type, :null => false # question, answer
+      t.boolean :to_flag, :null => false
       t.timestamps
     end
   end
