bc4595b2f176a21eaa7d6f20b6b404ea19b84428
Lorefnon <lorefnon@gmail.com>
2012-08-19 03:22:57 +0530

    Notify about creation of answers.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -14,26 +14,13 @@
 #  updated_at     :datetime         not null
 #
 
-# == Schema Information
-#
-# Table name: answers
-#
-#  id          :integer          not null, primary key
-#  body        :text             default(""), not null
-#  creator_id  :integer          not null
-#  question_id :integer          not null
-#  upvotes     :integer          not null
-#  downvotes   :integer          not null
-#  is_flagged  :boolean          not null
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#
 require "opinable"
 require "commentable"
 require "subscribable"
 
 class Answer < ActiveRecord::Base
   include Authority::Abilities
+  after_save :post_save
   self.authorizer_name = "AnswersAuthorizer"
 
   attr_accessible :body
@@ -42,6 +29,7 @@ class Answer < ActiveRecord::Base
   has_many :comments
 
   has_many :moderators, :through => :question
+  has_many :activities, :as => :subject
 
   is_opinable
   is_commentable
@@ -51,4 +39,19 @@ class Answer < ActiveRecord::Base
   default_value_for :upvote_count, 0
   default_value_for :is_flagged, false
   default_value_for :is_active, true
+
+  def post_save
+    desc = if created_at_changed? then "created" else "edited" end
+
+    activity = Activity.create :subject => self,
+    :description => desc
+
+    q_subscribers = self.question.subscribers.all
+    a_subscribers = self.subscribers.all
+
+    (q_subscribers + a_subscribers).each do |subscriber|
+      Notification.create :user => subscriber, :activity => activity
+    end
+
+  end
 end
