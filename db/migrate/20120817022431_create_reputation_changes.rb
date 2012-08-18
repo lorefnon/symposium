897cabddb2c55f41626fc68a05dffb9835a54f7d
Lorefnon <lorefnon@gmail.com>
2012-08-19 03:16:29 +0530

    Define database schema for reputation change.

--- db/migrate/20120817022431_create_reputation_changes.rb
+++ db/migrate/20120817022431_create_reputation_changes.rb
@@ -1,7 +1,9 @@
 class CreateReputationChanges < ActiveRecord::Migration
   def change
     create_table :reputation_changes do |t|
-
+      t.integer :user_id
+      t.integer :change
+      t.integer :activity_id
       t.timestamps
     end
   end
