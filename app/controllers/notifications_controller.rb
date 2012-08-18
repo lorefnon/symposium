82e5e1a1e85782d848557633c7eb452e9ea2c1e7
Lorefnon <lorefnon@gmail.com>
2012-08-19 03:18:50 +0530

    Create index view for notifications.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,12 @@
 class NotificationsController < ApplicationController
+  def index
+    return unless protect_against_missing :member_id do
+      @user = User.find params[:member_id]
+      authorize_action_for @user
+    end
+    @notifications = @user.notifications
+    @target
+    respond_with @notifications
+    return
+  end
 end
