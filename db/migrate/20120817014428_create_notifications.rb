72a1caeaf4b4ff32719bf4a7dff7ce3844eca555
Lorefnon <lorefnon@gmail.com>
2012-08-19 03:14:52 +0530

    Remove multi-user notifications in favor of shared actions and single user notification.

--- db/migrate/20120817014428_create_notifications.rb
+++ db/migrate/20120817014428_create_notifications.rb
@@ -1,12 +1,11 @@
 class CreateNotifications < ActiveRecord::Migration
   def change
     create_table :notifications do |t|
-      t.timestamps
-    end
-
-    create_table :notifications_users do |t|
-      t.integer :notification_id
       t.integer :user_id
+      t.integer :priority
+      t.integer :activity_id
+      t.boolean :is_read
+      t.timestamps
     end
   end
 end
