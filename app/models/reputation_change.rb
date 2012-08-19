55ec3bb27bf4b5995dd80d6dabdcca067a708f38
Lorefnon <lorefnon@gmail.com>
2012-08-19 10:12:12 +0530

    Add accessible attributes to reputation change and update annotations.

--- app/models/reputation_change.rb
+++ app/models/reputation_change.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: reputation_changes
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer
+#  change      :integer
+#  activity_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class ReputationChange < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :change
+  belongs_to :user
+  belongs_to :activity
 end
--- spec/models/reputation_change_spec.rb
+++ spec/models/reputation_change_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: reputation_changes
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer
+#  change      :integer
+#  activity_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'spec_helper'
 
 describe ReputationChange do
