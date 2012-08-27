01fa32b8c97718c77f5d2e117acec1ed3e0bfef3
Lorefnon <lorefnon@gmail.com>
2012-08-27 11:05:39 +0530

    Add provision for listing of upvoters and downvoters.

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,7 +1,24 @@
 class MembersController < SymposiumBaseController
   def model_class; User end
 
+  def index_opinionators
+    @search_disabled = true
+    begin
+      target_class = params[:opinion_tgt_type].constantize
+      target = target_class.find params[:opinion_tgt_id]
+      @users = target.send(params[:op_classifier])
+      @hline = "#{params[:op_classifier]} for this #{target_class} are :"
+      respond_with @users
+    rescue
+      return declare_not_found
+    end
+  end
+
   def index
+    if params.has_key? :opinion_tgt_type and params.has_key? :opinion_tgt_id
+      return index_opinionators
+    end
+
     @users = User.includes(:tag_priviledges => [:tag])
     if params.has_key? :user_name and not params[:user_name].empty?
       sql = "user_name LIKE ?"
