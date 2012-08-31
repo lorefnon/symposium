0ac8982aa9b5920b4e5ae24f5c49bf379884ad7f
Lorefnon <lorefnon@gmail.com>
2012-08-31 16:45:32 +0530

    Add authorization rules for modification of tags.

--- app/authorizers/tags_authorizer.rb
+++ app/authorizers/tags_authorizer.rb
@@ -2,8 +2,20 @@ class TagsAuthorizer < ApplicationAuthorizer
   def self.default(adjective, user)
     user.is_active
   end
-  def self.creatable_by? (user)
+  def self.creatable_by? user
     true
     #user.reputation > 10
+  end
+  def modifiable_by? user
+    resource.creator.id == user.id or
+      ( user.role == "moderator" and
+        resource.moderators.exists? :id => user.id ) or
+      ( user.role == "admin" )
+  end
+  def editable_by? user
+    modifiable_by? user
+  end
+  def updatable_by? user
+    modifiable_by? user
   end
 end
