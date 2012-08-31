d30a20a5eaa976b63168351e6fe4007f006fd36b
Lorefnon <lorefnon@gmail.com>
2012-08-31 16:40:47 +0530

    Update opinion helper to provide proper labels for upvote/downvote buttons.

--- app/helpers/opinions_helper.rb
+++ app/helpers/opinions_helper.rb
@@ -2,22 +2,32 @@ module OpinionsHelper
   def opinion_box target
     already_upvoted = target.upvoters.include? current_user
     already_downvoted = target.downvoters.include? current_user
+    already_flagged = target.flaggers.include? current_user
     already_voted = already_upvoted or already_downvoted
 
     locals = {
       :target => target,
       :upvote_count => target.upvotes.length,
       :downvote_count => target.downvotes.length,
-      :upvote_title => "Upvote this answer",
-      :downvote_title => "Downvote this answer",
+      :upvote_title => "Upvote",
+      :downvote_title => "Downvote",
+      :flag_title => "Flag",
       :upvote_class => "",
       :downvote_class => "",
-      :method => if already_voted then "put" else "post" end
+      :upvote_method => if already_upvoted then "delete"
+                        elsif already_voted then "put"
+                        else "post" end,
+      :downvote_method => if already_downvoted then "delete"
+                          elsif already_voted then "put"
+                          else "post" end,
+      :flag_method => if already_flagged then "delete"
+                      elsif already_voted then "put"
+                      else "post" end
     }
 
     if not user_signed_in?
-      locals[:upvote_title] = "You need to login to " + locals[:upvote_title]
-      locals[:downvote_title] = "You need to login to " + locals[:downvote_title]
+      locals[:upvote_title] = "login to " + locals[:upvote_title]
+      locals[:downvote_title] = "login to " + locals[:downvote_title]
       locals[:upvote_class] = "disabled"
       locals[:downvote_class] = "disabled"
     elsif already_upvoted
@@ -26,6 +36,8 @@ module OpinionsHelper
     elsif already_downvoted
       locals[:downvote_title] = "Undo Downvote"
       locals[:downvote_class] = "chosen"
+    elsif already_flagged
+      locals[:flag_title] = "Undo flag"
     end
 
     render :partial => "opinions/opinion_box", :locals => locals
