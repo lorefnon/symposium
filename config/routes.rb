dea140dde774a95e165df9a12e761b84a01d7f3d
Lorefnon <lorefnon@gmail.com>
2012-08-31 16:42:00 +0530

    Update routes for restfulling managing opinions and moderators in a better way.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Symposium::Application.routes.draw do
   resources :reputation_changes
   resources :activities
   resources :opinions
+  resources :tags
 
   resources :questions, :shallow => true do
     resources :answers
@@ -23,71 +24,20 @@ Symposium::Application.routes.draw do
     end
   end
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
   root :to => 'questions#index'
 
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
-
   match '/community' => 'members#index'
-  match '/members/:member_id/dashboard' => 'members#dashboard'
+  match '/moderators/:id/dashboard' => 'moderators#dashboard'
+  match '/admin/:id/dashboard' => 'admin#dashboard'
 
   match '/:target_type/:target_id/opinion/:optype' => 'opinions#create',
   :via => :post
 
   match '/:target_type/:target_id/opinion/:optype' => 'opinions#update',
   :via => :put
+
+  match '/:target_type/:target_id/opinion/:optype' => 'opinions#delete',
+  :via => :delete
 
   match '/questions/:id/accept/:answer_id' => 'questions#accept_ans',
   :via => :put
@@ -98,6 +48,21 @@ Symposium::Application.routes.draw do
 
   match '/:opinion_tgt_type/:opinion_tgt_id/downvoters' => 'members#index',
   :optype => "downvote",
+  :via => :get
+
+  match '/tags/:id/moderators/:moderator_id' => 'tags#add_moderator',
+  :via => :post,
+  :constraints => { :moderator_id => /\d*/ }
+
+  match '/tags/:id/moderators/:moderator_id' => 'tags#remove_moderator',
+  :via => :delete,
+  :constraints => { :moderator_id => /\d*/ }
+
+  match '/tags/:id/moderators' => 'tags#add_moderator',
+  :via => :post,
+  :constraints => { :moderator_name => /[a-zA-Z_-]*/ }
+
+  match '/moderator/:moderator_id/tags' => 'tags#index',
   :via => :get
 
   match '/members/:member_id/subscribed/:target_type' => 'subscriptions#index',
